05212ba5e79734e91ec15447acc2df0b1104b7d3
MarcosJochatoma <you@example.com>
2017-05-12 16:29:40 -0500

    Segundo

---
+++ HelpDesk/HelpDesk.Entities/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Entities
+{
+    public class Persona
+    {
+        public int CodigoId { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String Apellido { get; set; }
+
+        public String Contraseña { get; set; }
+
+
+
+    }
+}
---
+++ HelpDesk/HelpDesk.Persistence/HelpDeskDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HelpDesk.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Persistence
+{
+    class HelpDeskDbContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
